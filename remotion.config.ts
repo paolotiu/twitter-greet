a9c8b9d16bd51b68caf6d627e147d6865f6da3b6
Paolo Tiu <paolotiu17@gmail.com>
2021-08-12 16:14:41 +0800

    chore: use all threads

--- remotion.config.ts
+++ remotion.config.ts
@@ -1,4 +1,8 @@
+import os from 'os';
 import {Config} from 'remotion';
 
 Config.Rendering.setImageFormat('jpeg');
 Config.Output.setOverwriteOutput(true);
+
+// Use all threads
+Config.Rendering.setConcurrency(os.cpus.length);
